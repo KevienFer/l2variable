6cc67a3dbef126f8fad4241dd0c4d89db9833c96
KevienFer <kevinferna98@gmail.com>
2024-05-26 16:42:40 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  document.getElementById('banner').style.display = 'block';
+}, 3000);
+setTimeout(() => {
+  document.getElementById('banner').style.display = 'none';
+}, 6000);
+
+setTimeout(() => {
+  document.getElementById('banner2').style.display = 'block';
+}, 3000);
+setTimeout(() => {
+  document.getElementById('banner2').style.display = 'none';
+}, 6000);
+setTimeout(() => {
+  document.getElementById('banner3').style.display = 'block';
+}, 3000);
+setTimeout(() => {
+  document.getElementById('banner3').style.display = 'none';
+}, 6000);
+setTimeout(() => {
+  document.getElementById('banner4').style.display = 'block';
+}, 3000);
+setTimeout(() => {
+  document.getElementById('banner4').style.display = 'none';
+}, 6000);
+
+
+
+const button = document.getElementById("boton1");
+function action() {
+  alert("¡Que esperas para ir a descargar nuestros archivos!");
+}
+button.addEventListener("click", action);
+
+
+
